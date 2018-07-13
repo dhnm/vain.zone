2ed9ea7005204a8b5de184936ddc36f16411198c
lukasdinh <MinhBook@MinhBook-Pro.local>
2018-07-13 12:58:44 +0200

    debug

--- modules/api/index.ts
+++ modules/api/index.ts
@@ -1,37 +1,36 @@
-import { Router, static as router_static } from "express";
-const path = require("path");
+import { Router, static as router_static } from 'express';
+const path = require('path');
 const router: Router = Router();
 
-import fbattachment from "./fbattachment";
-import botuser from "./botuser";
-import matches from "./matches";
-import telemetry from "./telemetry";
-import applyfilter from "./applyfilter";
-import bestczsk from "./bestczsk";
-import testing from "./testing";
+import fbattachment from './fbattachment';
+import botuser from './botuser';
+import matches from './matches';
+import telemetry from './telemetry';
+import applyfilter from './applyfilter';
+import bestczsk from './bestczsk';
+import testing from './testing';
 
-import uuidv4 from "./uuidv4";
+import uuidv4 from './uuidv4';
 
 export default router;
 
-router.use("/fbattachment", fbattachment);
-router.use("/botuser", botuser);
-router.use("/matches", matches);
-router.use("/telemetry", telemetry);
-router.use("/applyfilter", applyfilter);
-router.use("/bestczsk", bestczsk);
+router.use('/fbattachment', fbattachment);
+router.use('/botuser', botuser);
+router.use('/matches', matches);
+router.use('/telemetry', telemetry);
+router.use('/applyfilter', applyfilter);
+router.use('/bestczsk', bestczsk);
 router.use(
-	"/rankimage/c",
-	router_static(
-		path.join(process.env.PWD, "../../static/img/rank/c")
-	)
+  '/rankimage/c',
+  router_static(path.join(process.env.PWD, '/static/img/rank/c')),
 );
 console.log(
-	"keylog",
-	process.cwd(),
-	path.join(process.env.PWD, "../../static/img/rank/c"),
-	path.normalize(path.join(process.env.PWD, "../../static/img/rank/c"))
+  'keylog',
+  process.env.PWD,
+  process.cwd(),
+  path.join(process.env.PWD, '/static/img/rank/c'),
+  path.normalize(path.join(process.env.PWD, '/static/img/rank/c')),
 );
-router.use("/testing", testing);
+router.use('/testing', testing);
 
-router.use("/uuidv4", uuidv4);
+router.use('/uuidv4', uuidv4);
