f0139c959841f3b3cf0e4b4b61e948aeac8a09d6
lukasdinh <MinhBook@MinhBook-Pro.local>
2018-07-16 22:17:37 +0200

    functional /top

--- modules/api/percentrank.ts
+++ modules/api/percentrank.ts
@@ -36,11 +36,13 @@ router.get("/:IGN", (req: Request, res: Response): void => {
         res.send(`
           <!doctype html>
           <html prefix="og: http://ogp.me/ns#">
-          <head>
+          <head prefix>
           <title>VAIN.ZONE TOP</title>
-          <meta property="og:title" content="Jsem v Top ${Math.round(
-            100 - playersPercentRank
-          )} %!" />
+          <meta property="fb:app_id" content="617200295335676" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="${
+            player.name
+          } je v TOP ${Math.round(100 - playersPercentRank)} %!" />
           <meta property="og:description" content="${
             player.name
           } je lepší než ${Math.round(
@@ -49,9 +51,15 @@ router.get("/:IGN", (req: Request, res: Response): void => {
           <meta property="og:url" content="https://vain.zone/top/${
             player.name
           }" />
-          <meta property="og:image" content="https://vain.zone/api/rankimage/${
+          <meta property="og:image" content="https://vain.zone/api/rankimage/c/${
             processedSkillTier.number
           }${processedSkillTier.color}" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="300" />
+          <meta property="og:image:height" content="300" />
+          <meta property="og:image:alt" content="${processedSkillTier.name}${
+          processedSkillTier.color
+        }" />
           </head>
           <body>
           ${player.name} je lepší než ${Math.round(
@@ -71,6 +79,8 @@ router.get("/:IGN", (req: Request, res: Response): void => {
           <html prefix="og: http://ogp.me/ns#">
           <head>
             <title>VAIN.ZONE TOP</title>
+            <meta property="fb:app_id" content="617200295335676" />
+            <meta property="og:type" content="website" />
             <meta property="og:title" content="${
               req.params.IGN
             } není v databázi." />
@@ -79,6 +89,10 @@ router.get("/:IGN", (req: Request, res: Response): void => {
               req.params.IGN
             }?err=true" />
             <meta property="og:image" content="https://vain.zone/static/img/draft/logo.png" />
+            <meta property="og:image:type" content="image/png" />
+            <meta property="og:image:width" content="250" />
+            <meta property="og:image:height" content="250" />
+            <meta property="og:image:alt" content="VAIN.ZONE" />
           </head>
           <body>
             <a href="https://m.me/VAIN.ZONE">Klikni sem a vyzkoušej náš Messenger Bot!</a>
